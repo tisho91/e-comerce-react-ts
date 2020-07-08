47c0cdd1682d769ed0f64e64b65145cd0dcf7f61
Tihomir Marinov <tihomir.j.marinov@gmail.com>
2020-07-08 11:37:22 +0300

    homepage

--- src/homepage.component.tsx
+++ src/homepage.component.tsx
@@ -1,14 +1,38 @@
 import React from "react";
+import './homepage.styles.scss'
 
 const HomePage = () => (
     <div className='homepage'>
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">
-                        Hats
-                    </h1>
-                    <span className="subtitle">Shop now</span>
+        <h1>Welcome to my Homepage</h1>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>HATS</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>JACKETS</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>SHOES</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>WOMENS</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>MENS</div>
+                    <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
         </div>
