237d6842cc6a79abb3497c56671ff6c500327701
Tihomir Marinov <tihomir.j.marinov@gmail.com>
2020-07-14 12:34:39 +0300

    8. Form Input Component

--- src/componnents/login/login.component.tsx
+++ src/componnents/login/login.component.tsx
@@ -2,6 +2,7 @@ import React, {ChangeEvent, Component} from 'react';
 import {TLogin} from "@types";
 
 import './login.styles.scss'
+import FormInput from "../form-input/form-input.component";
 
 class Login extends Component<any,any> {
     constructor(props:TLogin) {
@@ -32,11 +33,8 @@ class Login extends Component<any,any> {
                 <span>Login with email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.handleChange} name="email" id='email' type="email" value={this.state.email} required/>
-                    <label htmlFor="email">Email</label>
-                    <input onChange={this.handleChange} name="password" id='password' type="password" value={this.state.password} required/>
-                    <label htmlFor="password">Password</label>
-                    <input type="submit" value="Submit Form"/>
+                    <FormInput handleChange={this.handleChange} label='email' name="email" id='email' type="email" value={this.state.email} required/>
+                    <FormInput handleChange={this.handleChange} label='password++66' name="password" id='password' type="password" value={this.state.password} required/>
                 </form>
             </div>
         );
